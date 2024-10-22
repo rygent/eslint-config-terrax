863a75c9f3f93cdfbfffbee0697be278029eee5f
Rygent <46462403+rygent@users.noreply.github.com>
2024-10-23 06:54:15 +0700

    chore: cleanup

--- src/jsx-a11y.ts
+++ src/jsx-a11y.ts
@@ -1,5 +1,6 @@
 import { fixupPluginRules } from '@eslint/compat';
 import type { TSESLint } from '@typescript-eslint/utils';
+// @ts-expect-error eslint-plugin-jsx-a11y is not typed
 import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y';
 
 const rules: TSESLint.FlatConfig.Rules = {
