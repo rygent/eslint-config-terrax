03f45eff623265d4ebf9a2fdbe42319dece871b4
Rygent <46462403+rygent@users.noreply.github.com>
2024-11-14 09:55:08 +0700

    chore: fix config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 import tseslint from 'typescript-eslint';
 import common from './dist/esm/common.mjs';
-import node from './dist/esm/node.mjs';
 import typescript from './dist/esm/typescript.mjs';
-import stylistic from './dist/esm/stylistic-typescript.mjs';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default tseslint.config(...common, ...node, ...typescript, ...stylistic, {
+export default tseslint.config(...common, ...typescript, {
 	languageOptions: {
 		parserOptions: {
 			project: './tsconfig.eslint.json'
